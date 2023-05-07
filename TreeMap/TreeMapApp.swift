8961c7ea4126dfc556e57dd1d19329708fd7bd96
samekard_dev <88572321+samekard-dev@users.noreply.github.com>
2023-05-07 23:12:26 +0900

    Initial Commit

---
+++ TreeMap/TreeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeMapApp.swift
+//  TreeMap
+//
+//  Created by MH on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct TreeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
